a2242106bfc9e10c040f02c04454cb57cc97d4eb
Alexy Lafosse <149681987+XLA17@users.noreply.github.com>
2025-11-24 11:22:17 +0100

    add: independant structure

--- Assets/Scripts/Voxel.cs
+++ Assets/Scripts/Voxel.cs
@@ -1,87 +1,148 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Voxel : MonoBehaviour
+public class SphereVoxel : MonoBehaviour
 {
     [SerializeField] private GameObject cubePrefab;
-    [SerializeField] private float radius;
-    [SerializeField] private int depth;
+    [SerializeField] private Vector3 octreeCenter;
+    [SerializeField] private float octreeWidth;
+    [SerializeField] private int octreeDepth;
+    [SerializeField] private List<Sphere> spheres;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Recursif(depth, transform.position, radius*2, radius*2);
+
+        Octree octree = new(octreeCenter, octreeWidth, octreeDepth, spheres);
+
+        RecursiveInstantiate(octree.rootNode.nodes);
+    }
+
+    void RecursiveInstantiate(List<OctreeNode> nodes)
+    {
+        Debug.Log("RecursiveInstantiate");
+        foreach(OctreeNode node in nodes)
+        {
+            if (node.depth <= 0)
+            {
+                GameObject cube = Instantiate(cubePrefab, node.center, Quaternion.identity);
+                cube.transform.localScale = Vector3.one * node.scale;
+                cube.transform.parent = transform;
+            }
+            else
+            {
+                RecursiveInstantiate(node.nodes);
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
     }
+}
 
-    //List<Vector3> GetCube(Vector3 center, float width)
-    //{
-    //    List<Vector3> cube = new List<Vector3>();
-    //    cube.Add(new Vector3(center.x - width / 2, center.y - width / 2, center.z - width / 2));
-    //    cube.Add(new Vector3(center.x - width / 2, center.y - width / 2, center.z + width / 2));
-    //    cube.Add(new Vector3(center.x - width / 2, center.y + width / 2, center.z - width / 2));
-    //    cube.Add(new Vector3(center.x - width / 2, center.y + width / 2, center.z + width / 2));
-    //    cube.Add(new Vector3(center.x + width / 2, center.y - width / 2, center.z - width / 2));
-    //    cube.Add(new Vector3(center.x + width / 2, center.y + width / 2, center.z - width / 2));
-    //    cube.Add(new Vector3(center.x + width / 2, center.y - width / 2, center.z + width / 2));
-    //    cube.Add(new Vector3(center.x + width / 2, center.y + width / 2, center.z + width / 2));
+[System.Serializable]
+public class Sphere
+{
+    public Vector3 center;
+    public float radius;
 
-    //    return cube;
-    //}
+    public Sphere(Vector3 center, float radius)
+    {
+        this.center = center;
+        this.radius = radius;
+    }
+}
+
+public class Octree
+{
+    private Vector3 center;
+    private float width;
+    private int depth;
+    public OctreeNode rootNode;
+    public List<Sphere> spheres;
+
+    public Octree(Vector3 center, float width, int depth, List<Sphere> spheres)
+    {
+        this.center = center;
+        this.width = width;
+        this.depth = depth;
+        this.spheres = spheres;
+
+        rootNode = new(center, width, width, depth, spheres); // mouais
+    }
+}
+
+public class OctreeNode
+{
+    private List<Sphere> spheres;
+    public Vector3 center;
+    private float width;
+    public float scale;
+
+    public int depth;
+    public List<OctreeNode> nodes;
+
+    public OctreeNode(Vector3 center, float width, float scale, int depth, List<Sphere> spheres)
+    {
+        this.center = center;
+        this.width = width;
+        this.scale = scale;
+        this.depth = depth;
+        this.spheres = spheres;
+
+        nodes = CreateNodes();
+    }
 
     List<Vector3> GetCentersOf8Cubes(Vector3 center, float width)
     {
-        List<Vector3> cube = new List<Vector3>();
-        cube.Add(new Vector3(center.x - width / 4, center.y - width / 4, center.z - width / 4));
-        cube.Add(new Vector3(center.x - width / 4, center.y - width / 4, center.z + width / 4));
-        cube.Add(new Vector3(center.x - width / 4, center.y + width / 4, center.z - width / 4));
-        cube.Add(new Vector3(center.x - width / 4, center.y + width / 4, center.z + width / 4));
-        cube.Add(new Vector3(center.x + width / 4, center.y - width / 4, center.z - width / 4));
-        cube.Add(new Vector3(center.x + width / 4, center.y + width / 4, center.z - width / 4));
-        cube.Add(new Vector3(center.x + width / 4, center.y - width / 4, center.z + width / 4));
-        cube.Add(new Vector3(center.x + width / 4, center.y + width / 4, center.z + width / 4));
+        List<Vector3> cube = new()
+        {
+            new Vector3(center.x - width / 4, center.y - width / 4, center.z - width / 4),
+            new Vector3(center.x - width / 4, center.y - width / 4, center.z + width / 4),
+            new Vector3(center.x - width / 4, center.y + width / 4, center.z - width / 4),
+            new Vector3(center.x - width / 4, center.y + width / 4, center.z + width / 4),
+            new Vector3(center.x + width / 4, center.y - width / 4, center.z - width / 4),
+            new Vector3(center.x + width / 4, center.y + width / 4, center.z - width / 4),
+            new Vector3(center.x + width / 4, center.y - width / 4, center.z + width / 4),
+            new Vector3(center.x + width / 4, center.y + width / 4, center.z + width / 4)
+        };
 
         return cube;
     }
 
-    void Recursif(int maxDepth, Vector3 center, float width, float scale)
+    List<OctreeNode> CreateNodes()
     {
-        if (maxDepth == 0)
+        if (depth <= 0)
         {
-            GameObject cube = Instantiate(cubePrefab, center, Quaternion.identity);
-            cube.transform.localScale = Vector3.one * scale;
-            cube.transform.parent = transform;
-            return;
+            return new();
         }
+
+        List<OctreeNode> nodes = new();
 
         List<Vector3> cubeCenters = GetCentersOf8Cubes(center, width);
 
         for (int i = 0; i < 8; i++)
         {
             Vector3 cubeCenter = cubeCenters[i];
-            float distanceBetweenCenters = Vector3.Distance(transform.position, cubeCenter);
             float cubeWidth = width / 2;
             float cubeScale = scale / 2;
 
-            if (distanceBetweenCenters + Mathf.Sqrt(3f) * cubeWidth / 2 < radius)
+            for (int j = 0; j < spheres.Count; j++)
             {
-                //GameObject cube = Instantiate(cubePrefab, cubeCenter, Quaternion.identity);
-                //cube.transform.localScale = Vector3.one * cubeScale;
-                //cube.transform.parent = transform;
-            }
-            else if (distanceBetweenCenters - Mathf.Sqrt(3f) * cubeWidth / 2 > radius)
-            {
-            }
-            else
-            {
-                Debug.Log(distanceBetweenCenters);
-                Debug.Log(Mathf.Sqrt(3) * cubeWidth);
-                Recursif(maxDepth - 1, cubeCenter, cubeWidth, cubeScale);
+                float distanceBetweenCenters = Vector3.Distance(spheres[j].center, cubeCenter);
+
+                if (distanceBetweenCenters + Mathf.Sqrt(3f) * cubeWidth / 2 >= spheres[j].radius && distanceBetweenCenters - Mathf.Sqrt(3f) * cubeWidth / 2 <= spheres[j].radius)
+                {
+                    OctreeNode node = new(cubeCenter, cubeWidth, cubeScale, depth - 1, spheres);
+                    nodes.Add(node);
+                    break;
+                }
             }
         }
+
+        return nodes;
     }
-}
+} 
